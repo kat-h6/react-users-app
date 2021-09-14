a1e5d8a132b7b6427a748afac8d011d074b51095
Katharine Hao <hao.katharine@gmail.com>
2021-09-14 16:22:54 -0400

    added circle in cards

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
 
 export default function User({ id, name, username, email }) {
     return (
         <div className="card">
+            <Avatar className="large-avatar">{name[0]}</Avatar>
             <h5>{name}</h5>
             <div className="card-details">
                 <p>@{username}</p>
